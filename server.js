ebe2c79bdf2f27ac2fb0bde6283090d79b4f7ed4
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-05-23 23:38:16 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// requires express
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+require("./routes/apiRoutes")(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`listening on PORT: http://localhost:${PORT}`));
+
